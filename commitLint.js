1afae46318839385f77303a0662f024d5fa7e167
rodrigo.lopez@continuum.cl <rodrigo.lopez@continuum.cl>
2021-08-31 20:39:49 -0500

    feat: initial config

---
+++ commitLint.js
@@ -0,0 +1,15 @@
+module.exports = {
+  types: [
+    { type: 'feat', section: 'Features', hidden: false },
+    { type: 'revert', section: 'Reverts', hidden: false },
+    { type: 'fix', section: 'Bug Fixes', hidden: false },
+    { type: 'docs', section: 'Documentation', hidden: false },
+    { type: 'perf', section: 'Performance Updates', hidden: false },
+    { type: 'chore', section: 'Others', hidden: true },
+    { type: 'test', section: 'Tests', hidden: false },
+    { type: 'ci', section: 'CI', hidden: false },
+    { type: 'refactor', section: 'Code Refactoring', hidden: false },
+    { type: 'style', section: 'Styling', hidden: true },
+    { type: 'build', section: 'Build System', hidden: false },
+  ],
+};
